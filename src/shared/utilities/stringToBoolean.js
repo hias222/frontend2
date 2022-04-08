21a160644615034e1bbc1ab1c7611a40b573afa9
matthias <matthias.fuchs>
2022-04-08 15:58:43 +0200

    error undefined stringToBoolean

--- src/shared/utilities/stringToBoolean.js
+++ src/shared/utilities/stringToBoolean.js
@@ -1,12 +1,16 @@
 export default function stringToBoolean(string) {
-    switch (string.toLowerCase().trim()) {
-        case "true":
-        case "yes":
-        case "1": return true;
-        case "false":
-        case "no":
-        case "0":
-        case null: return false;
-        default: return Boolean(string);
+    if (string === undefined) {
+        return false
+    } else {
+        switch (string.toLowerCase().trim()) {
+            case "true":
+            case "yes":
+            case "1": return true;
+            case "false":
+            case "no":
+            case "0":
+            case null: return false;
+            default: return Boolean(string);
+        }
     }
 }
