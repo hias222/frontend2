72777bf3630be45cd33421af8ba247e074dc9392
Matthias <matthias>
2025-06-15 17:54:38 +0200

    namen angepasst

--- src/naming/GetPageName.tsx
+++ src/naming/GetPageName.tsx
@@ -3,13 +3,15 @@ import { useParams } from "react-router-dom";
 function changeName(name: string): string {
 
     if (name === "maerz") {
-        return "Nürnberger Märzmeeting 2024"
-    } else if (name === "masters") {
-        return "Bayerische Kurzbahnmeisterschaft der Masters 2024"
+        return "Nürnberger Märzmeeting 2025"
+    } else if (name === "bayern") {
+        return "Bayerische Kurzbahnmeisterschaft 2025"
     } else if (name === "verein") {
         return "SG Fürth Vereinsmeisterschaft 2024"
     } else if (name === "bezirk") {
-        return "Bezirksmeisterschaften 2024"
+        return "Bezirksmeisterschaften 2025"
+    } else if (name === "dmsm") {
+        return "28. Deutscher Mannschaftswettbewerb 2025"
     } else {
         return name
     }
