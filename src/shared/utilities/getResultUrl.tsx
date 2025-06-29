67bf4a46cfb3c384bc089bb7c626c25f5d396096
Matthias <matthias>
2025-06-29 21:48:18 +0200

    update

--- src/shared/utilities/getResultUrl.tsx
+++ src/shared/utilities/getResultUrl.tsx
@@ -2,8 +2,8 @@
 function GetResultUrl(base_url: string |undefined, route_path: string | undefined): string  {
 
     if (route_path !== undefined && route_path === "bezirk") {
-        //return "https://easywk.swimdata.de/results.php"
-        return base_url + "/splashdata/" + route_path + "/results.html"
+        return "https://easywk.swimdata.de/results.php"
+        //return base_url + "/splashdata/" + route_path + "/results.html"
     } else {
         return base_url + "/splashdata/" + route_path + "/index.html"
     }
