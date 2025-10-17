12e6c8ecb455f18910faadbb189859dd1134ba27
Matthias <matthias>
2025-10-17 23:32:42 +0200

    update

--- src/shared/utilities/getClubUrl.tsx
+++ src/shared/utilities/getClubUrl.tsx
@@ -2,8 +2,8 @@
 function GetClubUrl(base_url: string |undefined, route_path: string | undefined): string  {
 
     if (route_path !== undefined && route_path === "bayern") {
-        //return "https://easywk.swimdata.de/results.php"
-        return base_url + "/splashdata/" + route_path + "/club/index.html"
+        return "https://easywk.swimdata.de/results.php"
+        //return base_url + "/splashdata/" + route_path + "/club/index.html"
     } else {
         return base_url + "/splashdata/" + route_path + "/club/index.html"
     }
